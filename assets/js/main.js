924a6760aefb36358bd04e65793b6cb22da31c06
Dash <dash.elhauge@gmail.com>
2022-07-30 19:21:26 -0400

    Updating style of font so more readable

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     document.title="Dashiel Carrera";
     
     $('.policy-box').html(`<ul>
-                <li><span>© Dashiel Carrera 2021. All rights reserved. </span></li>
+                <li><span>© Dashiel Carrera 2022. All rights reserved. </span></li>
                 <!--li><a href="mailto:dashiel.carrera@gmail.com">dashiel.carrera@gmail.com</a></li-->
               </ul>`);
     $('.fixed-footer-social').html(`<ul>
